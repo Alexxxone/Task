decc346c521207e9b2f8b2f745367a4c85b43981
Vordak <serejkaua@gmail.com>
2013-05-22 15:57:33 +0300

    Add task1

---
+++ task1.rb
@@ -0,0 +1,10 @@
+module Hello	
+	def description
+		print "Вы вести в консоли Hello \r\n"
+	end	
+	def show
+		12.times {|i| i == 5 ? puts("#{i+1})Hello word") : puts("#{i+1})Hello")}
+	end
+	description()
+	show()
+end
