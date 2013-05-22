915d4b5838d446600abec32ecf9ce7b6f3ce312f
Sergii Siniavin <serejkaua@gmail.com>
2013-05-22 18:32:37 +0300

    Update task1.rb

--- task1.rb
+++ task1.rb
@@ -1,6 +1,6 @@
 module Hello	
 	def description
-		print "Вы вести в консоли Hello \r\n"
+		print "Вывести в консоли Hello \r\n"
 	end	
 	def show
 		12.times {|i| i == 5 ? puts("#{i+1})Hello word") : puts("#{i+1})Hello")}
